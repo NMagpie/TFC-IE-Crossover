171840b4b7cf20efacbfa9e03a1ce42bdf7ec34f
HermitOwO <hermitcutest@gmail.com>
2023-05-06 11:25:41 +0800

    Add compatibility for external heater
    External heater can now heat a crucible

---
+++ src/main/java/com/nmagpie/tfc_ie_addon/compat/external_heater/IEHeatHandler.java
@@ -0,0 +1,66 @@
+package com.nmagpie.tfc_ie_addon.compat.external_heater;
+
+import blusunrize.immersiveengineering.api.tool.ExternalHeaterHandler;
+import com.nmagpie.tfc_ie_addon.config.Config;
+import com.nmagpie.tfc_ie_addon.config.ServerConfig;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+
+import net.dries007.tfc.common.capabilities.heat.HeatCapability;
+
+public class IEHeatHandler
+{
+    public static class CrucibleHeater implements ExternalHeaterHandler.IExternalHeatable
+    {
+        private final BlockEntity blockEntity;
+
+        public CrucibleHeater(BlockEntity blockEntity)
+        {
+            this.blockEntity = blockEntity;
+        }
+
+        @Override
+        public int doHeatTick(int energyAvailable, boolean redstone)
+        {
+            return blockEntity.getCapability(HeatCapability.BLOCK_CAPABILITY).map(handler ->
+            {
+                int rfPerTick = Config.SERVER.crucibleExternalHeaterRFperTick.get();
+                if (energyAvailable >= rfPerTick)
+                {
+                    handler.setTemperature(Config.SERVER.crucibleExternalHeaterTemperature.get());
+                    return rfPerTick;
+                }
+                return 0;
+            }).orElse(0);
+        }
+    }
+
+    public static class Provider implements ICapabilityProvider
+    {
+        private final CrucibleHeater heater;
+        private final LazyOptional<ExternalHeaterHandler.IExternalHeatable> lazy;
+
+        public Provider(BlockEntity blockEntity)
+        {
+            this.heater = new CrucibleHeater(blockEntity);
+            this.lazy = LazyOptional.of(() -> heater);
+        }
+
+        @Nonnull
+        @Override
+        public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+            return ExternalHeaterHandler.CAPABILITY.orEmpty(cap, lazy);
+        }
+
+        public void invalidate() {
+            lazy.invalidate();
+        }
+    }
+}
