6f46e4491fc0ad1d1e6206f888fe4b426f0dd6d2
HermitOwO <hermitcutest@gmail.com>
2025-11-27 21:16:36 +0800

    Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val ieVersion: String = "1.21.1-12.4.2-194"
 val dualCodecsVersion: String = "0.1.2"
 
 val modId: String = "tfc_ie_addon"
-val modVersion: String = "2.0.4"
+val modVersion: String = "2.0.5"
 val modJavaVersion: String = "21"
 val modIsInCI: Boolean = !modVersion.contains("-indev")
 val modDataOutput: String = "src/generated/resources"
