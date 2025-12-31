c9c80b1b91b863780ad24d70df40b3d58a27f151
HermitOwO <hermitcutest@gmail.com>
2025-12-31 23:25:11 +0800

    Fix Fluid Outlet recipe, closes #31

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ val modId: String = "tfc_ie_addon"
 base {
     archivesName.set("TFC-IE-Crossover-$minecraftVersion")
     group = "com.nmagpie.tfc_ie_addon"
-    version = "1.3.0"
+    version = "1.3.1"
 }
 
 java {
